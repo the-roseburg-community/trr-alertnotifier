284ecee751505372bfb63c7f3d82fa8882eef822
Joseph Ziegler <joseph@josephziegler.com>
2023-09-23 22:55:26 -0700

    Add updated code

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM python:3.9.18-alpine3.17
 
 WORKDIR /app
 
-RUN pip3 install requests
+RUN pip3 install requests mailersend
 
 COPY . .
--- alert-check.py
+++ alert-check.py
@@ -1,47 +1,116 @@
 # Author: Joseph Ziegler
-#
+# Email: joseph@josephziegler.com
+# Description:
+#   This python application sends email-based text messages to all numbers listed in the numbers.txt file if
+#   a specific feed contains an alert.
 
 import os
-import requests # pip
+import requests                # Install with pip
+from mailersend import emails  # Install with pip
 
-PASSWORD = os.getenv('PASSWORD')
-USERNAME = os.getenv('USERNAME')
+from_address = "noreply@roseburg.city"
+
+PASSWORD = os.getenv('PASSWORD')                        # Environment variable for Broadcastify feed owner password
+USERNAME = os.getenv('USERNAME')                        # Environment variable for Broadcastify feed owner username
+mailer = emails.NewEmail(os.getenv('MAILSEND_API_KEY')) # Environment variable for Mailersend API Key
+feed_id = "40833"                                       # Broadcastify Feed ID
+
+# Phone carrier dictionary
+carrier_mapping = {
+  "googlefi": "msg.fi.google.com",
+  "verizon": "vtext.com",
+  "tmobile": "tmomail.net",
+  "att": "txt.att.net",
+  "spectrum": "vtext.com",
+  "xfinity": "vtext.com",
+  "uscellular": "email.uscc.net"
+}
 
 api_url = "https://api.broadcastify.com/owner/"
 
-try:
-  with open('feeds.txt', 'r') as file:
-    for line in file:
-      feed = line.strip().split()
-      if len(feed) >= 2:
-        feed_id = int(feed[0])
-        emails = feed[1:]
+def mail(subject, content):
+  mail_body = {}
+  try:
+    with open('numbers.txt', 'r') as file:
+      mail_list = []  # Initialize mail_list
+      for line in file:
+        number = line.strip().split()
+        if len(number) >= 2:  # Make sure that both the number and carrier exist in the text file
+          phone_number = int(number[0])
+          provider = number[1]
+          print(phone_number)
+          print(provider)
+          if provider in carrier_mapping:
+            adjusted_carrier = carrier_mapping[provider]
+            email = str(phone_number) + "@" + adjusted_carrier
+            print("Email is %s" % email)
 
-        full_feed_url = api_url + "?a=feed&feedId=" + str(feed_id) + "&type=json&u=" + USERNAME + "&p=" + PASSWORD
-        payload={}
-        headers = {}  
-        response = requests.request("GET", full_feed_url, headers=headers, data=payload)
-        if response.status_code == 200:
-          data = response.json()
+            # Create a new email_info dictionary for each email
+            email_info = {
+              "from": {
+                "email": from_address,
+                "name": "The Roseburg Receiver"
+              },
+              "to": [
+                {
+                  "email": email,
+                  "name": "TRR Member"
+                }
+              ],
+              "subject": subject,
+              "text": content,
+            }
 
-          content = data["Feed"][0]
-          
-          counties = content["Counties"]
-          county_info = counties[0]
-
-          feed_name = content["descr"]
-          listeners = content["listeners"]
-          county_name = county_info['name']
-          county_state = county_info["stateName"]
-
-
-          print("Feed Name: %s, %s County - %s" % (feed_name,county_name,county_state))
-          print("Listeners: %d" % listeners)
+            mail_list.append(email_info)
         else:
-          print("Failed to retrieve data from the API. Status code:", response.status_code)
+          print("Invalid line:", line)
+    print(mail_list)
+    # print(mailer.send_bulk(mail_list))  # Send Email
+  except Exception as e:
+    print(e)
 
-      else:
-        print("Invalid line:", line)
+
+## Script start
+state_file_path = "/state/previous_state.txt" # Use a state file in order to understand if the alert already sent a text or if the alert message has changed.
+try:
+  with open(state_file_path, "r") as state_file:
+    previous_state = state_file.read().strip()
+except FileNotFoundError:
+  previous_state = None
+
+try:
+  full_feed_url = api_url + "?a=feed&feedId=" + str(feed_id) + "&type=json&u=" + USERNAME + "&p=" + PASSWORD
+  payload = {}
+  headers = {}
+  response = requests.request("GET", full_feed_url, headers=headers, data=payload)
+  if response.status_code == 200:
+    data = response.json()
+
+    content = data["Feed"][0]
+
+    counties = content["Counties"]
+    county_info = counties[0]
+
+    feed_name = content["descr"]
+    listeners = content["listeners"]
+    county_name = county_info['name']
+    county_state = county_info["stateName"]
+
+    if int(listeners) > 40:  # If there is an alert
+      #current_value = data["specific_field"] # Data for the alert message
+      #if previous_state is None or previous_state != current_value:
+        print("%s %s" % (feed_name, str(listeners)))
+        mail("Feed " + feed_name + " has an alert!", "Feed " + feed_name + " has an alert! It also has " + str(listeners) + " listeners. This is a TEST ONLY. This is not a real alert.")
+        print("Feed Name: %s, %s County - %s" % (feed_name, county_name, county_state))
+        print("Listeners: %d" % listeners)
+
+        # Save the updated state to the file
+        with open(state_file_path, "w") as state_file:
+          state_file.write(str(previous_state))
+  else:
+    print("Failed to retrieve data from the API. Status code:",
+      response.status_code)
 except Exception as error:
   print(error)
+
 
